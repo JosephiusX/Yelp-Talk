edf428470087a23c727f2952072155be7a4b5f59
Joseph <josephiusx@gmail.com>
2021-06-07 11:49:52 -0700

    added doccumentation to app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const path = require('path'); // re quire path for app.set below
 const mongoose = require('mongoose');
-const Comment = require('./models/comment')
+const Comment = require('./models/comment') // require comment schema file from models dir
 
-mongoose.connect('mongodb://localhost:27017/talk-app', {
+mongoose.connect('mongodb://localhost:27017/talk-app', { // mongoose connection 
     useNewUrlParser: true, 
     useCreateIndex: true,
     useUnifiedTopology: true
 });
 
-const db = mongoose.connection;
+const db = mongoose.connection; // mongoose connection
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
@@ -23,33 +23,33 @@ app.set('views', path.join(__dirname, 'views')) // sets path to absolute
 
 app.use(express.urlencoded({ extended: true})) // tell express to parse the body
 
-app.get('/', (req, res) => {
-    res.render('home')
+app.get('/', (req, res) => { // home route
+    res.render('home') // home.ejs
 })
 
-app.get('/comments', async(req, res) => {
-    const comments = await Comment.find({})
-    res.render('comments/index', {comments})
+app.get('/comments', async(req, res) => { // comments route
+    const comments = await Comment.find({}) // find comments from database
+    res.render('comments/index', {comments}) // index.ejs, giving access to comments obj 
 })
 
-app.get('/comments/new', (req, res) => {
-    res.render('comments/new')
+app.get('/comments/new', (req, res) => { // new comment route
+    res.render('comments/new') // new.ejs
 })
 
-app.post('/comments', async(req, res) => {
-    const comment = new Comment(req.body.comment);
-    await comment.save();
-    res.redirect(`/comments/${comment._id}`)
+app.post('/comments', async(req, res) => { // save route
+    const comment = new Comment(req.body.comment); 
+    await comment.save(); // save comment to db
+    res.redirect(`/comments/${comment._id}`) // redirects to comment with the id we just created
 })
 
 app.get('/comments/:id', async(req, res) => { // show route
-    const comment = await Comment.findById(req.params.id)
-    res.render('comments/show', { comment })
+    const comment = await Comment.findById(req.params.id) // find the comment with this id
+    res.render('comments/show', { comment }); // show.ejs , gives accdss to comment obj
 })
 
-app.get('/comments/:id/edit', async(req, res) => {
-    const comment = await Comment.findById(req.params.id)
-    res.render('comments/edit', { comment })
+app.get('/comments/:id/edit', async(req, res) => { // edit route
+    const comment = await Comment.findById(req.params.id) // find a comment with this id
+    res.render('comments/edit', { comment }); // edit.ejs gives access to comment
 })
 
 app.listen(3000, () => {
